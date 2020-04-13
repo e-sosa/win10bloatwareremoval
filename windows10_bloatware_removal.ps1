b98e64c5c813235502eecb9537bf0333310a4b27
e-sosa <30646085+e-sosa@users.noreply.github.com>
2020-04-12 20:47:38 -0600

    Add files via upload

---
+++ windows10_bloatware_removal.ps1
@@ -0,0 +1,143 @@
+﻿Get-AppXPackage -allusers *3dbuilder* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*3dbuilder*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *windowsalarms* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*windowsalarms*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *AppConnector* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*AppConnector*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *windowscommunicationsapps* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*windowscommunicationsapps*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *WindowsCamera* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*WindowsCamera*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *WindowsFeedbackHub* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*WindowsFeedbackHub*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *MicrosoftOfficeHub* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*MicrosoftOfficeHub*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *SkypeApp* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*SkypeApp*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *Getstarted* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*Getstarted*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *ZuneMusic* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*ZuneMusic*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *ZuneVideo* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*ZuneVideo*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *WindowsMaps* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*WindowsMaps*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *Messaging* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*Messaging*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *MicrosoftSolitaireCollection* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*MicrosoftSolitaireCollection*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *Wallet* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*Wallet*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *ConnectivityStore* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*ConnectivityStore*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *bingnews* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*bingnews*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *onenote* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*onenote*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *oneconnect* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*oneconnect*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *people* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*people*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *CommsPhone* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*CommsPhone*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *Photos* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*Photos*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *MicrosoftStickyNotes* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*MicrosoftStickyNotes*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *WindowsStore* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*WindowsStore*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *Sway* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*Sway*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *XboxApp* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*XboxApp*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *XboxIdentityProvide* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*XboxIdentityProvide*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *WindowsSoundRecorder* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*WindowsSoundRecorder*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *StorePurchaseApp* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*StorePurchaseApp*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *BingWeather* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*BingWeather*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *DesktopAppInstaller* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*DesktopAppInstaller*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *PPIProjection* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*PPIProjection*"} | remove-appxprovisionedpackage -online
+ 
+Get-AppXPackage -allusers *Microsoft3DViewer* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*Microsoft3DViewer*"} | remove-appxprovisionedpackage -online
+ 
+Get-AppXPackage -allusers *MSPaint* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*MSPaint*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *XboxGameOverlay* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*XboxGameOverlay*"} | remove-appxprovisionedpackage -online
+ 
+Get-AppXPackage -allusers *XboxSpeechToTextOverlay* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*XboxSpeechToTextOverlay*"} | remove-appxprovisionedpackage -online
+ 
+ 
+Get-AppXPackage -allusers *HoloCamera* | Remove-AppXPackage
+Get-AppXProvisionedPackage -online | where-object {$_.packagename -like "*HoloCamera*"} | remove-appxprovisionedpackage –online
